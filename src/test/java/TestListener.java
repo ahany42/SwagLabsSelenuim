94d4aba1e7c7a03ef87e626dc8de13093079f64a
ahany42 <ahanyfathy42@gmail.com>
2025-07-27 21:59:30 +0300

    Log Screenshot

--- src/test/java/TestListener.java
+++ src/test/java/TestListener.java
@@ -12,26 +12,20 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
-        // Get the test class instance to access WebDriver
         Object testClass = result.getInstance();
 
-        // Cast it to your BaseTest to access getDriver()
         WebDriver driver = ((BaseTest) testClass).getDriver();
 
         if (driver != null) {
-            // Take screenshot
             File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             try {
-                // Ensure the folder exists
                 File screenshotsDir = new File("screenshots");
                 if (!screenshotsDir.exists()) {
                     screenshotsDir.mkdirs();
                 }
 
-                // Create destination file with test name
                 File destFile = new File(screenshotsDir, result.getName() + ".png");
 
-                // Copy the screenshot to destination
                 Files.copy(screenshot.toPath(), destFile.toPath());
 
                 System.out.println("Saved failed test screenshot: " + destFile.getAbsolutePath());
